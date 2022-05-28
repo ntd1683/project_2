cda50bad2b69bbb776f16d378410a1a2c46e179a
MinhChau999 <89113312+MinhChau999@users.noreply.github.com>
2022-05-28 15:30:56 +0700

    make model route_driver_car -a and create migrate seeder

---
+++ app/Http/Controllers/RouteDriverCarController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Route_driver_car;
+use App\Http\Requests\StoreRoute_driver_carRequest;
+use App\Http\Requests\UpdateRoute_driver_carRequest;
+
+class RouteDriverCarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreRoute_driver_carRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreRoute_driver_carRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Route_driver_car  $route_driver_car
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Route_driver_car $route_driver_car)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Route_driver_car  $route_driver_car
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Route_driver_car $route_driver_car)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateRoute_driver_carRequest  $request
+     * @param  \App\Models\Route_driver_car  $route_driver_car
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateRoute_driver_carRequest $request, Route_driver_car $route_driver_car)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Route_driver_car  $route_driver_car
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Route_driver_car $route_driver_car)
+    {
+        //
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,5 +23,6 @@ class DatabaseSeeder extends Seeder
         $this->call(CarriageSeeder::class);
         $this->call(SeatSeeder::class);
         $this->call(BillSeeder::class);
+        $this->call(RouteDriverCarSeeder::class);
     }
 }
