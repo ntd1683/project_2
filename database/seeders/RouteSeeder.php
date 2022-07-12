f7cc7cf30f28db1425fe3ba96b697f4ee51757a8
Nguyễn Minh Châu <89113312+MinhChau999@users.noreply.github.com>
2022-07-12 13:26:35 +0700

    update(seed): ReouteSeeder

--- database/seeders/RouteSeeder.php
+++ database/seeders/RouteSeeder.php
@@ -22,12 +22,21 @@ class RouteSeeder extends Seeder
         for ($i = 1; $i <= 20; $i++) {
             $city_start_id = $faker->randomElement($city);
             $city_end_id = $faker->randomElement($city);
+            $time = $faker->numberBetween(1, 48);
+            $distance = $faker->numberBetween(100, 1000);
             $arr[] = [
                 'city_start_id' => $city_start_id,
                 'city_end_id' => $city_end_id,
                 'name' => City::query()->where('id', $city_start_id)->value('name') . ' - ' . City::query()->where('id', $city_end_id)->value('name'),
-                'time' => $faker->numberBetween(1, 48),
-                'distance' => $faker->numberBetween(15, 200),
+                'time' => $time,
+                'distance' => $distance,
+            ];
+            $arr[] = [
+                'city_start_id' => $city_end_id,
+                'city_end_id' => $city_start_id,
+                'name' => City::query()->where('id', $city_end_id)->value('name') . ' - ' . City::query()->where('id', $city_start_id)->value('name'),
+                'time' => $time,
+                'distance' => $distance,
             ];
         }
         Route::insert($arr);
