459c8eaa1ed0950285f0c34faddb74fedbbcc39c
ntd1683 <nguyentandung1683@gmail.com>
2022-08-17 23:15:57 +0700

    Update HomePageController.php

--- app/Http/Controllers/Applicant/HomePageController.php
+++ app/Http/Controllers/Applicant/HomePageController.php
@@ -279,16 +279,6 @@ class HomePageController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
     public function payment(StoreInfoCustomerRequest $request)
     {
         $request->bus = json_decode($request->bus,true);
