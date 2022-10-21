fc14ac61b61a1c0300cec13b3d84a358754fcc4d
Nguyễn Minh Châu <89113312+MinhChau999@users.noreply.github.com>
2022-10-21 21:02:05 +0700

    update(schedule): crud

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -6,7 +6,9 @@ use App\Enums\CarriageCategoryEnum;
 use App\Enums\CarriageColorEnum;
 use App\Enums\SeatTypeEnum;
 use App\Http\Requests\StoreScheduleRequest;
+use App\Models\Route_driver_car;
 use App\Models\Schedule;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Str;
@@ -51,7 +53,7 @@ class ScheduleController extends Controller
     {
         $route_id = $request->get('route_id');
         return $this->model
-            ->select('schedules.id as id', 'schedules.day_of_week as day_of_week', 'schedules.time_of_day as time_of_day', 'schedules.pin_double_week as pin_double_week',
+            ->select('schedules.id as id', 'schedules.day_of_week as day_of_week', 'schedules.time_of_day as time_of_day',
                     'carriages.license_plate as license_plate', 'carriages.id as car_id', 'carriages.color as color',
                     'route_driver_cars.route_id as route_id')
             ->join('route_driver_cars', 'route_driver_cars.id', '=', 'schedules.route_driver_car_id')
@@ -67,8 +69,43 @@ class ScheduleController extends Controller
     public function store(StoreScheduleRequest $request)
     {
         $str = $request['data'];
-        // $array = explode(',', $str);
+        $route_id = $request['route_id'];
+        // $convert string to array json
         $array = json_decode($str, TRUE);
-        return $array[0];
+
+                
+        // Delete all records that are not in array
+        try {
+            $this->model
+                ->join('route_driver_cars', 'route_driver_cars.id', '=', 'schedules.route_driver_car_id')
+                ->where('route_driver_cars.route_id', $route_id)
+                ->whereNotIn('schedules.id', array_column($array, 'id'))->delete();
+        } catch(\Exception $e) {
+            return $this->errorResponse("Lưu thất bại");
+        }
+        // Create or update schedules
+        try {
+            foreach($array as $value){
+                $RDC_id = Route_driver_car::query()->where('route_id', $route_id)->where('car_id', $value['car_id'])->first()->id;
+                if($value['id'] == 0){
+                    $this->model->create([
+                        'route_driver_car_id' => $RDC_id,
+                        'day_of_week' => $value['day_of_week'],
+                        'time_of_day' => $value['time_of_day'],
+                    ]);
+                } else {
+                    $this->model->where('schedules.id', $value['id'])
+                    ->update([
+                        'route_driver_car_id' => $RDC_id,
+                        'day_of_week' => $value['day_of_week'],
+                        'time_of_day' => $value['time_of_day'],
+                    ]);
+                }
+            }
+        } catch(\Exception $e) {
+            return $this->errorResponse("Lưu thất bại");
+        }
+
+        return $this->successResponse([],"Lưu thành công");
     }
 }
--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Schedule extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        "route_driver_car_id",
+        "day_of_week",
+        "time_of_day",
+    ];
 }
